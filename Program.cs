943190ea365ae5f377601c877eb10f155deefde3
abhishekbmro <abhishek.singh@bulkmro.com>
2020-06-11 14:02:38 +0530

    successfully read the deliveries data, converted to DataTable and printed in console

--- Program.cs
+++ Program.cs
@@ -12,8 +12,21 @@ namespace IPLDataProject
         {
             var matches = ReadMatches();
 
-            //print data in matches data table
+            //print data in matches DataTable
             foreach (DataRow dataRow in matches.Rows)
+            {
+                foreach (var item in dataRow.ItemArray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine("-----------------------------------=======");
+
+            var deliveries = ReadDeliveries();
+
+            //print data in deliveries DataTable
+            foreach (DataRow dataRow in deliveries.Rows)
             {
                 foreach (var item in dataRow.ItemArray)
                 {
@@ -33,6 +46,25 @@ namespace IPLDataProject
                     var matchesataTable = new DataTable();
                     matchesataTable.Load(dr);
                     return matchesataTable;
+                }
+            }
+        }
+
+        static DataTable ReadDeliveries()
+        {
+            string filePath = Path.GetFullPath(@"IPLDataset/deliveries.csv");
+            using (var reader = new StreamReader(filePath))
+            using (var csvObject = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                using (var dr = new CsvDataReader(csvObject))
+                {
+                    var deliveriesTable = new DataTable();
+                    // deliveries.Columns.Add("wide_runs", typeof(int));
+                    // deliveries.Columns.Add("noball_runs", typeof(int));
+                    // deliveries.Columns.Add("extra_runs", typeof(int));
+                    // deliveries.Columns.Add("total_runs", typeof(int));
+                    deliveriesTable.Load(dr);
+                    return deliveriesTable;
                 }
             }
         }
