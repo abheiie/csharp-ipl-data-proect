466762ba39d1958959d2a00c3b51587ab66f6b21
abhishekbmro <abhishek.singh@bulkmro.com>
2020-06-11 13:38:52 +0530

    successfully reads the matches.csv, converted datatable object and printed in console

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Data;
+using CsvHelper;
 
 namespace IPLDataProject
 {
-    class Program
+    class IPLAnalysis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var matches = ReadMatches();
+
+            //print data in matches data table
+            foreach (DataRow dataRow in matches.Rows)
+            {
+                foreach (var item in dataRow.ItemArray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        static DataTable ReadMatches()
+        {
+            string filePath = Path.GetFullPath(@"IPLDataset/matches.csv");
+            using (var reader = new StreamReader(filePath))
+            using (var csvObject = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                using (var dr = new CsvDataReader(csvObject))
+                {
+                    var matchesataTable = new DataTable();
+                    matchesataTable.Load(dr);
+                    return matchesataTable;
+                }
+            }
         }
     }
 }
